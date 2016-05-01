649c9256ea109f6575bee3854ac656dec1675685
Edward Banner <edward.banner@gmail.com>
2016-05-01 09:43:11 -0500

    solution

--- codeforces/489/489c.py
+++ codeforces/489/489c.py
@@ -1,68 +1,36 @@
-import operator
-
-def do_dp(dp, cmp, init):
-    M, S = len(dp)-1, len(dp[0])-1
-
-    for m in range(2, M):
-        for s in range(1, S+1):
-            
-            best_num = init
-            for digit in range(10):
-                if s-digit < 0:
-                    continue
-
-                if not dp[m-1][s-digit]:
-                    continue
-
-                num = str(digit) + dp[m-1][s-digit]
-                if cmp(int(num), int(best_num)):
-                    best_num = num
-
-            dp[m][s] = str(best_num) if not best_num == init else ''
-
-    # Last row - no leading zeros
-    best_num, s, m = init, S, M
-    for s in range(1, S+1):
-
-        best_num = init
-        for digit in range(1, 10):
-            if s-digit < 0:
-                continue
-
-            if not dp[M-1][s-digit]:
-                continue
-
-            num = str(digit) + dp[M-1][s-digit]
-            if cmp(int(num), int(best_num)):
-                best_num = num
-
-        dp[M][s] = str(best_num) if not best_num == init else ''
-
-    return dp[M][S]
+import sys
 
 
 if __name__ == '__main__':
     M, S = [int(num) for num in raw_input().split()]
 
-    # Min case
-    dp = [['']*(S+1) for _ in range(M+1)]
-
-    for i in range(S+1)[:10]:
-        dp[1][i] = str(i)
-
-    for i in range(1, M+1):
-        dp[i][0] = '0'*i
-
-    if M == 1:
-        min_num = max_num = dp[1][S]
-    else:
-        min_num = do_dp(dp, operator.lt, init=1e100)
-        max_num = do_dp(dp, operator.gt, init=0)
-
     if M == 1 and S == 0:
-        min_num = max_num = 0
-    else:
-        min_num = -1 if not min_num or min_num.startswith('0') else min_num
-        max_num = -1 if not max_num or max_num.startswith('0') else max_num
+        print 0, 0
+        sys.exit()
 
-    print min_num, max_num
+    max_num, s = '', S
+    for _ in range(M): # build largest string greedily
+        if s < 9:
+            max_num += str(s)
+            s = 0 # zeros from here on out
+
+        else:
+            max_num += str(9)
+            s -= 9 # append nines as long as possible
+
+    if max_num.startswith('0') or s > 0:
+        print -1, -1
+        sys.exit()
+
+    min_num = list(reversed(max_num))
+
+    if min_num[0] == '0': # fix leading zero if there is one
+        for i in range(M):
+            if min_num[i] == '0':
+                continue
+
+            min_num[0] = '1'
+            min_num[i] = str(int(min_num[i]) - 1)
+            break
+
+    print ''.join(min_num), max_num
