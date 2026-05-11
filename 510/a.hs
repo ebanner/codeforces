1111fb03623a4fe0fa75bfc0a0b4108627b36437
Edward Banner <edward.banner@gmail.com>
2026-05-11 15:29:25 -0400

    🟢 510. Fox and Snake

--- 510/a.hs
+++ 510/a.hs
@@ -1,4 +1,4 @@
-import Data.Array (Array, listArray)
+import Control.Monad (forM_)
 import Data.List (intercalate)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -46,76 +46,77 @@ getGrid (n, m) = Map.fromList $
     [((i,j), '.') | i <- [0..n-1], j <- [0..m-1]]
 
 
-data Direction = North | East | South | West deriving (Eq)
+data Direction = North | East | South | West deriving (Eq, Show)
 
 type Index = (Int, Int)
 
 draw :: Grid Char -> Index -> Direction -> Int -> Grid Char
 draw grid (i,j) direction ticks
-  | direction == South && i == n-1 = grid
-
   | direction == South && j == m-1 && ticks == 0 =
       draw
         (Map.insert (i,j) '#' grid)
         (i,j)
         West
-        -1
+        (-1)
 
   | direction == South && j == 0 && ticks == 0 =
       draw
         (Map.insert (i,j) '#' grid)
         (i,j)
         East
-        -1
+        (-1)
+
+  | direction == South && i == n-1 = grid
+
+  | direction == South =
+      draw
+        (Map.insert (i,j) '#' grid)
+        (i+1,j)
+        South
+        (ticks-1)
 
   | direction == East && j == m-1 =
       draw
         (Map.insert (i,j) '#' grid)
         (i,j)
         South
-        3
+        2
 
-  | direction == South && i == n-1 =
-      draw
-        (Map.insert (i,j) '#' grid)
-        (i,j)
-        West
-        -1
-
-  | direction == West && i == 0 =
+  | direction == West && j == 0 =
       draw
         (Map.insert (i,j) '#' grid)
         (i,j)
         South
-        3
+        2
 
   | direction == East =
       draw
-        (Map.insert (i,j+1) '#' grid)
-        (i,j)
+        (Map.insert (i,j) '#' grid)
+        (i,j+1)
         direction
-        -1
+        (-1)
 
   | direction == West =
       draw
-        (Map.insert (i,j-1) '#' grid)
-        (i,j)
+        (Map.insert (i,j) '#' grid)
+        (i,j-1)
         direction
-        -1
-
-  | otherwise = error "Unreachable"
+        (-1)
 
   where
     (n, m) = (getHeight grid, getWidth grid)
 
 
 drawSnake :: Grid Char -> Grid Char
-drawSnake grid = draw grid (0,0) East
+drawSnake grid = draw grid (0,0) East (-1)
 
 
 main = do
   (n, m) <- getDimensions
 
-  let emptyGrid = getGrid (n, m)
+  let
+    emptyGrid = getGrid (n, m)
+    grid = drawSnake emptyGrid
 
-  drawSnake emptyGrid
+  forM_ [0..n-1] $ \i ->
+    putStrLn [(grid Map.! (i,j)) | j <- [0..m-1]]
